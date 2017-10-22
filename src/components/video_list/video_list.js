f0c0066a26ba5119bf4b2a27103aa4cf056c5923
Matteo Rossi <matteo.rossi@gruppometa.it>
2017-10-22 07:50:42 +0200

    Add video iframe open
    Add video iframe open on video select

--- src/components/video_list/video_list.js
+++ src/components/video_list/video_list.js
@@ -5,9 +5,18 @@ import VideoListItem from '../video_list_item/video_list_item';
 class VideoList extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      selected: false
+    };
   }
 
   render() {
+    const handelClick = selected => {
+      this.setState({ selected });
+      this.props.onVideoSelect(selected);
+    };
+
     if (this.props.videos.length === 0) {
       return (
         <p>Nessun video caricato</p>
@@ -18,7 +27,8 @@ class VideoList extends Component {
         {this.props.videos.map(video => {
           return (
             <VideoListItem
-              onVideoSelect={this.handleClick}
+              onVideoSelect={handelClick}
+              selected={this.state.selected}
               key={video.etag}
               video={video}
             />
--- src/components/video_list_item/video_list_item.js
+++ src/components/video_list_item/video_list_item.js
@@ -3,12 +3,32 @@ import React from 'react';
 import './video_list_item.scss';
 import { truncate } from '../utils/utils';
 
-const VideoListItem = ({ video, onVideoSelect }) => {
+const VideoListItem = ({ video, onVideoSelect, selected }) => {
   const imageUrl = video.snippet.thumbnails.default.url;
 
+  const renderIframeVideo = () => {
+    const selectedId = selected && selected.id ? selected.id.videoId : null;
+    const { videoId } = video.id;
+    if (videoId !== selectedId) {
+      return <div />;
+    }
+    const url = `https://www.youtube.com/embed/${videoId}`;
+    return (
+      <div className="box-iframeVideo embed-responsive embed-responsive-16by9">
+        <div className="box-header">
+          <button type="button" className="btn btn-xs btn-danger">
+            <span onClick={() => onVideoSelect(null)} className="fa fa-times" />
+          </button>
+        </div>
+        <iframe allowFullScreen className="embed-responsive-item" src={url} />
+      </div>
+    );
+  };
+
   return (
-    <div onClick={() => onVideoSelect(video)} className="video_list_item list-group-item">
-      <div className="media">
+    <div className="video_list_item list-group-item">
+      {renderIframeVideo()}
+      <div className="media" onClick={() => onVideoSelect(video)}>
         <div className="media_left">
           <img alt={video.snippet.title} className="media_object" src={imageUrl} />
         </div>
